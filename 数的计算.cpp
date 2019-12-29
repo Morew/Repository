71ba5a267ee76c9dd80f54303ad7bc038b284ad6
Morew <48649793+Morew@users.noreply.github.com>
2019-12-29 22:48:54 +0800

    Update 数的计算.cpp

--- 数的计算.cpp
+++ 数的计算.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 #define maxn 1000
 using namespace std;
 int main()
@@ -16,21 +16,4 @@ int main()
 		
 	}
 	cout << f[n];
-}*/
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-using namespace std;
-int n, f[1010];
-int main(){  
-  scanf("%d", &n);    
-  f[0] = f[1] = 1;   
-   for(int i = 2; i <= n; ++i)   
-    {       if(i%2==0)         
-		   			f[i] = f[i/2]+f[i-1];     
-		     else        
-			      f[i] = f[i-1];  
-  }   
-   printf("%d\n", f[n]);   
-    return 0;}
-
+}
